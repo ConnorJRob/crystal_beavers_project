aa38ea38bf4cf6bbd80b027c7fb70e2a50be3b3f
Connor <connorjrobertson@hotmail.co.uk>
2021-08-06 17:53:57 +0100

    temp seed_ref file set up

---
+++ seed_ref_delete.js
@@ -0,0 +1,95 @@
+topics["topic"]["subtopic"]["lesson"]
+{
+    "_id": "lkasdfklasdflkj2345345345",
+    "lesson-name": "Earth",
+    "lesson": [
+        "section1": {
+            "contents" {
+                "title": "title",
+                "image": "local url for images",
+                "text": "string"
+                },
+            "question": {
+                "question-text": "string",
+                "answers" : [
+                    {
+                        "answer-1-text": "string",
+                        "answer-1-value": 0/1
+                    },
+                    {
+                        "answer-2-text": "string",
+                        "answer-2-value": false
+                    },
+                    {
+                        "answer-3-text": "string",
+                        "answer-3-value": false
+                    },
+                    {
+                        "answer-4-text": "string",
+                        "answer-4-value": false
+                    },
+                ]
+            }
+        },
+        "section2": {
+            "contents" {
+                "title": "title",
+                "image": "local url for images",
+                "text": "string",
+                "atmosphere": {
+                    "oxygen": int,
+                    "hydrogen": int,
+                }
+                },
+            "question": {
+                "question-text": "string",
+                "answers" : [
+                    {
+                        "answer-1-text": "string",
+                        "answer-1-value": true
+                    },
+                    {
+                        "answer-2-text": "string",
+                        "answer-2-value": false
+                    },
+                    {
+                        "answer-3-text": "string",
+                        "answer-3-value": false
+                    },
+                    {
+                        "answer-4-text": "string",
+                        "answer-4-value": false
+                    },
+                ]
+            }
+        },
+        "section3": {
+            "contents" {
+                "title": "title",
+                "image": "local url for images",
+                "text": "string"
+                },
+            "question": {
+                "question-text": "string",
+                "answers" : [
+                    {
+                        "answer-1-text": "string",
+                        "answer-1-value": true
+                    },
+                    {
+                        "answer-2-text": "string",
+                        "answer-2-value": false
+                    },
+                    {
+                        "answer-3-text": "string",
+                        "answer-3-value": false
+                    },
+                    {
+                        "answer-4-text": "string",
+                        "answer-4-value": false
+                    },
+                ]
+            },
+        },
+    ]
+}
