9ad3fb4b7fd66bede7f29b8516391d23b74ccffb
David Macara Brown <davidmacarabrown@gmail.com>
2021-08-08 14:32:46 +0100

    lesson/section structure fixed bug 2

--- server/db/seed_data.js
+++ server/db/seed_data.js
@@ -7,14 +7,14 @@ db.placeholdername.insertOne(
 
         "background-image": "https://www.gannett-cdn.com/presto/2018/08/06/USAT/d7e9198a-b2fa-4ca6-b947-31c3751cc248-GettyImages-898660948.jpg?crop=5369,3020,x0,y0&width=3200&height=1680&fit=bounds",
 
-        "lesson": [
+        "lesson": {
                     "section-1": {
-                                    "contents" {
+                                    "contents": {
                                                 "title": "Earth",
                                                 "text": "that is a sweet earth"
-                                    }
-                    }
-                ]
+                                                }
+                                }
+                   }
             
     }
 );
