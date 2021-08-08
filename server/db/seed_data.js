b079bfb38bf68d5665baf9989ff95ead3bcd01ad
David Macara Brown <davidmacarabrown@gmail.com>
2021-08-08 15:26:30 +0100

    seed data and api get route working

--- server/db/seed_data.js
+++ server/db/seed_data.js
@@ -109,7 +109,7 @@ db.placeholdername.insertOne(
                                                 }
 
                                     }
-                                }
+                            }
                 }
             
     }
