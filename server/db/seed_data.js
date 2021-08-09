888f83d395a9dfbb091ff2adc725ab6b76709068
David Macara Brown <davidmacarabrown@gmail.com>
2021-08-09 14:25:25 +0100

    removed redundant field

--- server/db/seed_data.js
+++ server/db/seed_data.js
@@ -8,8 +8,6 @@ db.placeholdername.insertOne(
         "background_image": "https://www.gannett-cdn.com/presto/2018/08/06/USAT/d7e9198a-b2fa-4ca6-b947-31c3751cc248-GettyImages-898660948.jpg?crop=5369,3020,x0,y0&width=3200&height=1680&fit=bounds",
 
         "lesson": {
-                
-                    "title": "Earth",
 
                     "section_1": {
                                     "contents": {
