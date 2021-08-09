6bdc48e4ab6aa1b18817869dbf1250f8cbda7ab2
David Macara Brown <davidmacarabrown@gmail.com>
2021-08-09 17:42:47 +0100

    testing function

--- client/src/containers/Container.js
+++ client/src/containers/Container.js
@@ -8,9 +8,8 @@ const Container = function() {
     const [elementState, setElementState] = useState(0); //sets up a state that tracks lesson stages - modifying this allows us to iterate through them
     const [lessons, setLessons] = useState([]); // this state stores the data from our api (lessons collection)
     const [selectedLesson, setSelectedLesson] = useState(null); // this state is used in conjuection with the LessonSelector to define the currently selected lesson
-    const [selectedLessonSection1, setSelectedLessonSection1] = useState(null); 
-    const [selectedLessonSection2, setSelectedLessonSection2] = useState(null); 
-    const [selectedLessonSection3, setSelectedLessonSection3] = useState(null); 
+    const [lessonElements, setLessonElements] = useState([])
+
 
     useEffect(() => { // this runs when the page loads
         getLessons() // it calls the get Lessons function from LessonServices
@@ -19,36 +18,32 @@ const Container = function() {
         })
     }, []);
 
-    const chooseLesson = (lesson_id) => { // this function takes the lesson_id from the lessons selector when changed 
-        setSelectedLesson(lessons.forEach((lesson) => {
-            if (lesson_id === lesson._id){
-                setSelectedLessonSection1(lesson.sections[0]); 
-                setSelectedLessonSection2(lesson.sections[1]);
-                setSelectedLessonSection3(lesson.sections[2]);
-                return lesson
-            }
-        })) // the filter function then returns ONLY the lesson with a matching _id from the lessons state - which becomes the selectedLesson
-    };
 
     const incrementState = function() { 
         setElementState(elementState + 1);//this function increments the element state which allows us to progress through the lesson
     }
 
-    const lessonSectionOneContents = <LessonContent lesson={selectedLessonSection1}/>
-    // const lessonSectionOneQuestion = <LessonQuestion lesson={selectedLessonSection1}/>
-    const lessonSectionTwoContents = <LessonContent lesson={selectedLessonSection2}/>
-    // const lessonSectionTwoQuestion = <LessonQuestion lesson={selectedLessonSection2}/>
-    const lessonSectionThreeContents = <LessonContent lesson={selectedLessonSection3}/>
-    // const lessonSectionThreeQuestion = <LessonContent lesson={selectedLessonSection3}/>
+    // const lessonSectionOneContents = <LessonContent lesson={selectedLessonSection1}/>
+    // // const lessonSectionOneQuestion = <LessonQuestion lesson={selectedLessonSection1}/>
+    // const lessonSectionTwoContents = <LessonContent lesson={selectedLessonSection2}/>
+    // // const lessonSectionTwoQuestion = <LessonQuestion lesson={selectedLessonSection2}/>
+    // const lessonSectionThreeContents = <LessonContent lesson={selectedLessonSection3}/>
+    // // const lessonSectionThreeQuestion = <LessonContent lesson={selectedLessonSection3}/>
     
+    //map sections of selected lesson, creating an element for each with a unique ID and pushing it to the elementList
+
     const elementList = []
 
-    elementList.push(lessonSectionOneContents);
-    // elementList.push(lessonSectionOneQuestion);
-    elementList.push(lessonSectionTwoContents);
-    // elementList.push(lessonSectionTwoQuestion);
-    elementList.push(lessonSectionThreeContents);
-    // elementList.push(lessonSectionThreeQuestion);
+    const sectionGenerator = function(lesson) {
+        lesson.sections.forEach((section, index) => {
+            const contents = <LessonContent lesson={section} />
+            elementList.push(contents)
+        })
+    }
+
+    sectionGenerator(selectedLesson)
+    
+    
 
     return (
         <>
