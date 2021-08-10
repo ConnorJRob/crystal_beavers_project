8b4f72680b5fd78fb064dbfb5af6b66a82aaef26
David Macara Brown <davidmacarabrown@gmail.com>
2021-08-10 12:28:32 +0100

    working on radio button functions

--- client/src/components/questions/Answers.js
+++ client/src/components/questions/Answers.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 const Answers = function({answers}) {
+
+    const allAnswers = answers.map((answer) => {
+        return(
+            <>
+                <form>
+                    <label>{answer.text}</label>
+                    <input type="radio" ></input>
+                </form>
+            </>
+        );
+    });
+
     return(
         <>
-        <p>{answers[0].text}</p>
+        <p>{allAnswers}</p>
         </>
     )
 }
