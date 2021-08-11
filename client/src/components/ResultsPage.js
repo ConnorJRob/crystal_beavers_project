17729501f89d88dded8e7bad729e5d64e9c1f470
Connor <connorjrobertson@hotmail.co.uk>
2021-08-11 13:25:34 +0100

    psuedocode added for ResultsPage.js

--- client/src/components/ResultsPage.js
+++ client/src/components/ResultsPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const ResultsPage = function ({questionScore, potentialScore, returnHome}) {
+const ResultsPage = function ({questionScore, potentialScore, returnHome}) { // is given questionScore, potentialScore and the returnHome function to use
     return(
         <>
         <h3>Your results!</h3>
 
+        {/* This results page displays the question score / and the potential score for this lesson */}
         <h4>{questionScore}/{potentialScore}</h4>
+        {/* The button triggers the returnHome function that was passed down from the container - ending the lesson and resetting the app */}
         <button onClick={returnHome}>Return Home</button>
         </>
     )
